04852c26dd486222d67222486350a49cb2aa7f80
Ayush Pandey <ayushpandey0618@gmail.com>
2024-11-12 20:41:45 +0530

    translation

--- app.py
+++ app.py
@@ -1,30 +1,32 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from summary_logic import summarizer
-
+from summary_logic import translate_text
+from googletrans import LANGUAGES
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/detect_language', methods=['POST'])
+def detect_language():
+    data = request.json
+    rawtext = data.get("rawtext")
+    _, detected_lang = translate_text(rawtext)  # Detect language only
+    detected_lang_full = LANGUAGES.get(detected_lang, "Unknown Language")
+    return jsonify({"detected_lang": detected_lang_full})
 
-
-
-@app.route('/analyse', methods =['GET','POST'])
+@app.route('/analyse', methods=['POST'])
 def analyse():
     if request.method == 'POST':
         rawtext = request.form['rawtext']
-        summary, original_txt, len_orig_txt, len_summary =summarizer(rawtext)
-
-    return render_template('summary.html', summary=summary, original_txt=original_txt, len_orig_txt=len_orig_txt, len_summary=len_summary)
+        summary, original_txt, len_orig_txt, len_summary = summarizer(rawtext)
+        return render_template('summary.html', summary=summary, original_txt=original_txt, len_orig_txt=len_orig_txt, len_summary=len_summary)
     
-
 if __name__ == "__main__":
     app.run(debug=True)
-
--- summary_logic.py
+++ summary_logic.py
@@ -2,75 +2,40 @@ import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
 from heapq import nlargest
+from googletrans import Translator
 
-text = """Samsung recently cancelled its in-person PC 2021 event, instead, committing to an online-only format. The South Korean tech giant
-recently made it official, setting a time and date for the Samsung Galaxy MC Virtual Event. 
+# Initialize Translator
+translator = Translator()
 
-The event will be held on June 28 at 17:15 UTC (22:45 IST) and will be live-streamed on YouTube. In its release, Samsung says that it will 
-introduce its "ever-expanding Galaxy device ecosystem". Samsung also plans to present the latest technologies and innovation efforts in 
-relation to the growing importance of smart device security.
-
-Samsung will also showcase its vision for the future of smartwatches to provide new experiences for users and new opportunities for 
-developers. Samsung also shared an image for the event with silhouettes of a smartwatch, a smartphone, a tablet and a laptop."""
+def translate_text(text, dest_lang='en'):
+    # Detect language and translate to the specified language (English)
+    detected_lang = translator.detect(text).lang
+    translated_text = translator.translate(text, src=detected_lang, dest=dest_lang).text
+    return translated_text, detected_lang
 
 def summarizer(rawdocs):
-    stopwords=list(STOP_WORDS)
-    #print(stopwords)
+    # Step 1: Translate input text to English
+    rawdocs_english, original_lang = translate_text(rawdocs, 'en')
 
+    # Step 2: Summarization logic
+    stopwords = list(STOP_WORDS)
     nlp = spacy.load("en_core_web_sm")
-    doc = nlp(rawdocs)
-    #print(doc)
+    doc = nlp(rawdocs_english)
 
-    tokens = [token.text for token in doc]
-    #print(tokens)
-
-    word_freq ={}
+    word_freq = {}
     for word in doc:
         if word.text.lower() not in stopwords and word.text.lower() not in punctuation:
-            if word.text not in word_freq.keys():
-                word_freq[word.text] = 1
-            else:
-                word_freq[word.text] +=1
-
-    #print(word_freq)
+            word_freq[word.text] = word_freq.get(word.text, 0) + 1
 
     max_freq = max(word_freq.values())
-    #print(max_freq)
-
     for word in word_freq.keys():
-        word_freq[word] = word_freq[word]/max_freq
+        word_freq[word] = word_freq[word] / max_freq
 
-    #print(word_freq)
-
-    sent_tokens=[sent for sent in doc.sents]
-    #print(sent_tokens)
-
-    sent_scores ={}
-    for sent in sent_tokens:
-        for word in sent:
-            if word.text in word_freq.keys():
-                if sent not in sent_scores.keys():
-                    sent_scores[sent] = word_freq[word.text]
-                else:
-                    sent_scores[sent] += word_freq[word.text]
-
-    #print(sent_scores)
-
+    sent_tokens = [sent for sent in doc.sents]
+    sent_scores = {sent: sum(word_freq.get(word.text, 0) for word in sent) for sent in sent_tokens}
     select_len = int(len(sent_tokens) * 0.3)
-    #print(select_len)
+    summary = ' '.join([sent.text for sent in nlargest(select_len, sent_scores, key=sent_scores.get)])
 
-    summary = nlargest(select_len, sent_scores, key=sent_scores.get)
-
-    #print(summary)
-
-    final_summary =[word.text for word in summary]
-    summary = ' ' .join(final_summary)
-
-    #print(text)
-    #print(summary)
-
-    #print("length of original text",len(text.split(' ')))
-    #print("length of summary text",len(summary.split(' ')))
-
-    return summary, doc, len(rawdocs.split(' ')), len(summary.split(' '))
-
+    # Step 3: Translate summary back to the original language
+    summary_translated = translator.translate(summary, src='en', dest=original_lang).text
+    return summary_translated, rawdocs, len(rawdocs.split(' ')), len(summary_translated.split(' '))
